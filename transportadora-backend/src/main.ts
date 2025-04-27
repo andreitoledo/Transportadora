bf2bd35694842df8a1c0270edea1bba5ee1a4d6e
andreitoledo <andreiltoledo@hotmail.com>
2025-04-27 15:25:42 -0300

    criar a estrutura /common/ para melhorar o Swegger

--- transportadora-backend/src/main.ts
+++ transportadora-backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
@@ -18,10 +19,11 @@ async function bootstrap() {
   );
 
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle('Transportadora API')
-    .setDescription('Documentação das rotas da API')
+    .setDescription('Documentação oficial da API Transportadora')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
